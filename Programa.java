fddb7a61a884712a12bffe2e4a066bfa9f72c763
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-01-30 22:44:55 +0000

    Pruebas

---
+++ Programa.java
@@ -0,0 +1,10 @@
+import valores.*;
+
+public class Programa {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 6;
+    Abracadabra prueba = new Abracadabra();
+    System.out.println(prueba.suma(a, b));
+  }
+}
---
+++ valores/Abracadabra.java
@@ -0,0 +1,7 @@
+package valores;
+
+public class Abracadabra {
+  public int suma(int a, int b) {
+    return a + b;
+  }
+}
