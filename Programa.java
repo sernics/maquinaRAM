7eec4e0ec1ba1313505a5895d50d1d66c37353d6
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-01-31 22:39:20 +0000

    Output tape, little tests

--- Programa.java
+++ Programa.java
@@ -3,11 +3,21 @@ import Tapes.*;
 
 public class Programa {
   public static void main(String[] args) {
-    InputTape input = new InputTape("input.txt");
+    InputTape input = new InputTape("inputs/input.txt");
     // Imprimir input
     System.out.println("Input:");
     while (input.hasNext()) {
       System.out.println(input.read());
+    }
+    OutputTape output = new OutputTape();
+    // Escribir output
+    output.write(1);
+    output.write(2);
+    output.write(3);
+    // Imprimir output
+    System.out.println("Output:");
+    while (output.hasNext()) {
+      System.out.println(output.read());
     }
   }
 }
--- Tapes/OutputTape.java
+++ Tapes/OutputTape.java
@@ -3,10 +3,20 @@ import java.util.ArrayList;
 
 public class OutputTape {
   public OutputTape() {
+    head = 0;
     tape = new ArrayList<Integer>();
   }
   public void write(int value) {
     tape.add(value);
   }
+  public int read() {
+    int value = tape.get(head);
+    head++;
+    return value;
+  }
+  public boolean hasNext() {
+    return head < tape.size();
+  }
+  int head;
   private ArrayList<Integer> tape;
 }
