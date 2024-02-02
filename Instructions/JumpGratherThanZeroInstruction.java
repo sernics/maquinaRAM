0503527689af76c5d77a98c0e292adae1afc19d1
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-02-02 17:41:51 +0000

    Jump zero and jump grather than zero implements

---
+++ Instructions/JumpGratherThanZeroInstruction.java
@@ -0,0 +1,22 @@
+package Instructions;
+import ProgramData.*;
+import RegisterBank.DataMemory;
+
+public class JumpZeroInstruction implements Instruction {
+  public JumpZeroInstruction(Label label, Operator operator) {
+    this.label = label;
+    this.operator = operator;
+  }
+  public void execute(ProgramMemory programMemory, DataMemory dataMemory) {
+    if (dataMemory.containsRegister(0) && dataMemory.getRegister(0).getValue() > 0)
+      programMemory.setProgramCounter(programMemory.getLabelPosition(label));
+  }
+  public Label getLabel() {
+    return label;
+  }
+  public Operator getOperator() {
+    return operator;
+  }
+  Operator operator;
+  Label label;
+}
