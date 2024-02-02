b8702d23d4c5cc76b2e6511611a083cc66746ad2
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-02-02 09:03:00 +0000

    Added the main functions on the Instruction interface

--- Instructions/Instruction.java
+++ Instructions/Instruction.java
@@ -1,6 +1,10 @@
 package Instructions;
 import RegisterBank.DataMemory;
+import ProgramData.Label;
+import Operands.*;
 
 public interface Instruction {
   public void execute(DataMemory dataMemory);
+  public Label getLabel();
+  public BasicOperand getOperand();
 }
