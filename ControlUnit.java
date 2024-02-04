1cb5340645aa5a2b11942a3ef025cc34332de0c3
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-02-04 10:45:32 +0000

    Parse started

--- ControlUnit.java
+++ ControlUnit.java
@@ -3,10 +3,47 @@ import Operands.*;
 import ProgramMemory.ProgramMemory;
 import Registers.*;
 import Instructions.*;
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.*;
+import java.util.ArrayList;
 
 public class ControlUnit {
-  public ControlUnit(String filename) {
-    
+  public ControlUnit(String filename, String inputTape) {
+    try {
+      this.inputTape = new InputTape(inputTape);
+      this.outputTape = new OutputTape();
+      this.registerBank = new RegisterBank();
+      this.programMemory = new ProgramMemory();
+      Scanner scanner = new Scanner(new File(filename));
+      String pattern = "(?m)(?<=^|\\s)#.*$";
+      // Otro patron que sea muchos espacios a uno solo
+      String pattern2 = "\\s+";
+      Pattern p = Pattern.compile(pattern);
+      Pattern p2 = Pattern.compile(pattern2);
+      ArrayList<String> values = new ArrayList<String>();
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        Matcher m = p.matcher(line);
+        String lineWithoutComments = m.replaceAll("");
+        m = p2.matcher(lineWithoutComments);
+        lineWithoutComments = m.replaceAll(" ");
+        if (!lineWithoutComments.equals("")) {
+          String[] split = lineWithoutComments.split(" ");
+          for (String s : split) {
+            if (!s.equals("")) 
+              values.add(s);
+          }
+        }
+        // Imprimir values
+        for (String s : values) {
+          System.out.println(s);
+        }
+      }
+      scanner.close();
+    } catch (java.io.FileNotFoundException e) {
+      System.out.println("File not found: " + filename);
+    }
   }
   public void run() {
     while (true) {
--- Main.java
+++ Main.java
@@ -1,33 +1,5 @@
-import Tapes.*;
-import Operands.*;
-import Registers.*;
-import Instructions.*;
-import ProgramMemory.ProgramMemory;
-
-import java.util.ArrayList;
-
 public class Main {
   public static void main(String[] args) {
-    Tape tape = new InputTape("./inputs/input.txt");
-    RegisterBank registerBank = new RegisterBank();
-    BasicOperand operand = new DirectOperand(0);
-    Instruction instruction = new ReadInstruction(tape, registerBank, operand);
-    ProgramMemory programMemory = new ProgramMemory();
-    programMemory.add(instruction);
-    programMemory.add(instruction);
-    operand = new InmediateOperand(4);
-    instruction = new JumpGratherThanZeroInstruction(programMemory, operand, registerBank);
-    programMemory.add(instruction);
-    operand = new DirectOperand(0);
-    instruction = new AddInstruction(registerBank, operand);
-    programMemory.add(instruction);
-    instruction = new HaltInstruction();
-    programMemory.add(instruction);
-    while (true) {
-      System.out.println(programMemory.getHead());
-      System.out.println(registerBank.getRegister(0).getValue());
-      instruction = programMemory.read();
-      instruction.operate();
-    }
+    ControlUnit controlUnit = new ControlUnit("test/test2.ram", "inputs/input.txt");
   }
 }
