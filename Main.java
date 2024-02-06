3297f857f2d961654b2d8e5ea487121e49bd8384
Sergio Nicolas Segui <sergionicolassegui@macbookdesergio.lan>
2024-02-06 22:22:01 +0000

    Issue, i think the problem is on inmediate values

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    ControlUnit controlUnit = new ControlUnit("test/test4.ram", "inputs/input.txt", "outputs/output.txt");
+    ControlUnit controlUnit = new ControlUnit("test/testPow.ram", "inputs/input.txt", "outputs/output.txt");
     controlUnit.run();
   }
 }
