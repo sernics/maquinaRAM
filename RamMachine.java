4dfec9e5b0d88fe852d93757fd9eaba7992c3d51
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-02-17 17:32:34 +0000

    Translate the debug to a function

--- RamMachine.java
+++ RamMachine.java
@@ -9,30 +9,34 @@ public class RamMachine extends ControlUnit {
     run(0);
   }
 
+  public void debugg(Integer debugMode) {
+    if (debugMode == 2) {
+      System.out.println(getActualInstruction().toString());
+      getRegisterBank().print();
+      System.out.print("La cinta de entrada: ");
+      ArrayList<Integer> inputTape = getInputTape().getTape();
+      Integer inputTapePosition = getInputTape().getHead();
+      for (Integer value : inputTape) {
+        System.out.print(value + " ");
+      }
+      System.out.print("Actual Position -> " + inputTapePosition);
+      System.out.println();
+      // Cinta de salida
+      System.out.print("La cinta de salida: ");
+      ArrayList<Integer> outputTape = getOutputTape().getTape();
+      for (Integer value : outputTape) {
+        System.out.print(value + " ");
+      }
+      System.out.println("Actual Position -> " + getOutputTape().getHead());
+      System.out.println();
+    }
+  }
+
   public void run(Integer debugMode) {
     Integer instructionsCounter = 0;
     Integer haltInteger = getHaltInteger();
     while (getProgramHead() <= haltInteger) {
-      if (debugMode == 2) {
-        System.out.println(getActualInstruction().toString());
-        getRegisterBank().print();
-        System.out.print("La cinta de entrada: ");
-        ArrayList<Integer> inputTape = getInputTape().getTape();
-        Integer inputTapePosition = getInputTape().getHead();
-        for (Integer value : inputTape) {
-          System.out.print(value + " ");
-        }
-        System.out.print("Actual Position -> " + inputTapePosition);
-        System.out.println();
-        // Cinta de salida
-        System.out.print("La cinta de salida: ");
-        ArrayList<Integer> outputTape = getOutputTape().getTape();
-        for (Integer value : outputTape) {
-          System.out.print(value + " ");
-        }
-        System.out.println("Actual Position -> " + getOutputTape().getHead());
-        System.out.println();
-      }
+      debugg(debugMode);
       operate();
       instructionsCounter++;
     }
