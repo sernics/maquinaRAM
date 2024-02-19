89175748ce839c72b232547f7ead6d1260a71d00
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-02-19 08:32:16 +0000

    Terminar la práctica

--- Main.java
+++ Main.java
@@ -1,14 +1,7 @@
 import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> list = new ArrayList<>();
-    // Introducir 10 numeros aleatorios
-    for (int i = 0; i < 10; i++) {
-      list.add((int) (Math.random() * 100));
-    }
-    System.out.println("Original List: " + list);
-    RamMachine ramMachine = new RamMachine("test/insertion.ram", "inputs/input.txt", "outputs/output.txt");
-    ramMachine.setArray(list);
+    RamMachine ramMachine = new RamMachine("test/test7.ram", "inputs/input.txt", "outputs/output.txt");
     ramMachine.run(2);
   }
 }
