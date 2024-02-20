44a5383a505b4dacd67fc76b2f89383c2ae2f946
sernics <sernics@pachi>
2024-02-20 21:12:14 +0000

    Práctica terminada

--- MainArray.java
+++ MainArray.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class MainArray {
   public static void main(String[] args) {
     ArrayList<Integer> aleatoryNumbers = new ArrayList<Integer>();
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
       // Generar 10 números aleatorios
       aleatoryNumbers.add((int) (Math.random() * 100) + 1);
     }
-    RamMachine ramMachine = new RamMachine("test/insertion.ram", "inputs/input.txt", "outputs/output.txt");
+    RamMachine ramMachine = new RamMachine("test/bubble.ram", "inputs/input.txt", "outputs/output.txt");
     ramMachine.setArray(aleatoryNumbers);
     ramMachine.run(1);
   }
