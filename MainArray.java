0bc28aad3f1670980ac7cf63218db4bae6c8f97b
sernics <sernics@pachi>
2024-02-20 19:48:49 +0000

    Insertion sort work

--- MainArray.java
+++ MainArray.java
@@ -3,23 +3,12 @@ import java.util.ArrayList;
 public class MainArray {
   public static void main(String[] args) {
     ArrayList<Integer> aleatoryNumbers = new ArrayList<Integer>();
-    // Haz que aleatoryNumbers tenga los siguientes valores: {87, 12, 45, 67, 23, 98, 34, 56, 78, 10};
-    aleatoryNumbers.add(87);
-    aleatoryNumbers.add(12);
-    aleatoryNumbers.add(45);
-    aleatoryNumbers.add(67);
-    aleatoryNumbers.add(23);
-    aleatoryNumbers.add(98);
-    aleatoryNumbers.add(34);
-    aleatoryNumbers.add(56);
-    aleatoryNumbers.add(78);
-    aleatoryNumbers.add(10);
-    // for (int i = 0; i < 10; i++) {
-    //   // Generar 10 números aleatorios
-    //   aleatoryNumbers.add((int) (Math.random() * 100) + 1);
-    // }
+    for (int i = 0; i < 10; i++) {
+      // Generar 10 números aleatorios
+      aleatoryNumbers.add((int) (Math.random() * 100) + 1);
+    }
     RamMachine ramMachine = new RamMachine("test/insertion.ram", "inputs/input.txt", "outputs/output.txt");
     ramMachine.setArray(aleatoryNumbers);
-    ramMachine.run(2);
+    ramMachine.run(1);
   }
 }
---
+++ bubble.cc
@@ -0,0 +1,34 @@
+// 1  for i = 1 to n-1 do
+// 2     for j = n downto i + 1 do
+// 3        if A[j-1] > A[j] then begin
+// 4           temp = A[j-1]
+// 5           A[j-1] = A[j]
+// 6           A[j] = temp
+// 7        end
+// 8     end
+// 9  end
+#include <stdio.h>
+#include <iostream>
+
+void bubble_sort(int A[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    std::cout << "i: " << i << std::endl;
+    for (int j = n - 1; j > i; j--) {
+      if (A[j - 1] > A[j]) {
+        int temp = A[j - 1];
+        A[j - 1] = A[j];
+        A[j] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+  int A[] = {87, 12, 45, 67, 23, 98, 34, 56, 78, 10};
+  bubble_sort(A, 10);
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", A[i]);
+  }
+  printf("\n");
+  return 0;
+}
